0cb9a4e824dd7be0bce0f9f6b19e7e666bfe52e6
AmarKakde <amarkakde21@gmail.com>
2024-06-14 15:32:27 +0530

    whitenoise added

--- src/project/settings.py
+++ src/project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,14 +88,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-DATABASE_URL = config('DATABASE_URL', cast=str)
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL is not None:
     import dj_database_url
